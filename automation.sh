58635657f317363fefe244ebe8673cbe350c5d6b
chiranjeevi-am <chiranjeeviam@gmail.com>
2021-08-31 10:20:59 +0000

    Added code to view Bookkeeping (metadata of archived logs) and every day scheduled cron job

--- automation.sh
+++ automation.sh
@@ -43,3 +43,26 @@ aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
+size=$(du -sh /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -c 1-3)
+
+if [ ! -e /var/www/html/inventory.html ]
+then
+        echo "<!DOCTYPE html>
+        <html>
+        <head>
+        <title>My_webpage</title>
+        </head>
+        <body>
+        <h2>&emsp;&emsp;&emsp;Log Type&emsp;&emsp;&emsp;Time Created&emsp;&emsp;&emsp;Type&emsp;&emsp;&emsp;Size</h2>" > /var/www/html/inventory.html
+
+else
+	echo "<p style='font-size:23px'>&emsp;&emsp;&emsp;httpd-logs&emsp;&emsp;&emsp;${timestamp}&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;${size}</p>" >> /var/www/html/inventory.html
+fi
+
+chmod +x /root/Automation_Project/automation.sh
+if [ ! -e /etc/cron.d/automation ]
+then
+        echo "0 9 * * * root /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+else
+        echo "cron job scheduled already"
+fi
